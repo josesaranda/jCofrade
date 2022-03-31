fef8bc6d7a41ab00fbb26be6cadd80c969c29f1f
josesaranda <josesaranda@gmail.com>
2022-03-31 21:29:59 +0200

    About page initiation

---
+++ src/app/about/about.page.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { AboutPage } from './about.page';
+
+describe('AboutPage', () => {
+  let component: AboutPage;
+  let fixture: ComponentFixture<AboutPage>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [AboutPage],
+      imports: [IonicModule.forRoot(), RouterModule.forRoot([])]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AboutPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/about/about.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.page.html',
+  styleUrls: ['./about.page.scss'],
+})
+export class AboutPage implements OnInit {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private httpClient: HttpClient
+  ) { }
+
+  ngOnInit() {
+
+  }
+}
