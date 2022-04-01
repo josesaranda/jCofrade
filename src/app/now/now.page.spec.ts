0d0f56abb11a579d16932b6b2a095e45f60ea7d2
josesaranda <josesaranda@gmail.com>
2022-04-01 18:19:29 +0200

    Add initial logic and tests

--- src/app/now/now.page.spec.ts
+++ src/app/now/now.page.spec.ts
@@ -1,24 +1,93 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NowPage } from './now.page';
+import { APP_BASE_HREF } from '@angular/common';
+import { DBService } from '../db.service';
+import { instance, mock, when } from "ts-mockito"
+import { of } from 'rxjs';
+import { fakeDB } from '../fake/db';
 
 describe('NowPage', () => {
   let component: NowPage;
   let fixture: ComponentFixture<NowPage>;
+  const mockedDBService = mock(DBService)
 
-  beforeEach(waitForAsync(() => {
+  when(mockedDBService.getDB()).thenReturn(of(fakeDB))
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [NowPage],
-      imports: [IonicModule.forRoot(), RouterModule.forRoot([])]
-    }).compileComponents();
+      imports: [IonicModule.forRoot(), RouterModule.forRoot([])],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/my/app' },
+        { provide: DBService, useValue: instance(mockedDBService) }
+      ]
+    }).overrideTemplate(NowPage, "");
 
     fixture = TestBed.createComponent(NowPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe("#getBrotherhoodsNow", () => {
+    it("Should return desired value", () => {
+      const brotherhoods = (component as any).concatBrotherhoods(fakeDB);
+      (component as any).getBrotherhoodsNow(brotherhoods, 1648827095557 - 1000 * 60 * 10)
+      expect(component.brotherhoodsNow).toEqual([]);
+      (component as any).getBrotherhoodsNow(brotherhoods, 1648827095557 + 1000 * 60 * 10)
+      expect(component.brotherhoodsNow).toEqual([]);
+      (component as any).getBrotherhoodsNow(brotherhoods, 1648827095557 - 1000 * 60 * 3)
+      expect(component.brotherhoodsNow).toEqual([
+        {
+          name: 'test beggining domingo',
+          schedule: { name: 'Salida', datetime: 1648827095557 }
+        },
+        {
+          name: 'test finish',
+          schedule: { name: 'Encierro', datetime: 1648827095557 }
+        },
+        {
+          name: 'test beggining and tribune',
+          schedule: { name: 'Salida', datetime: 1648827095557 }
+        },
+        {
+          name: 'test beggining and tribune',
+          schedule: { name: 'Tribuna', datetime: 1648827095557 }
+        },
+        {
+          name: 'test beggining and tower',
+          schedule: { name: 'Salida', datetime: 1648827095557 }
+        },
+        {
+          name: 'test beggining and tower',
+          schedule: { name: 'Torre sur', datetime: 1648827095557 }
+        },
+        {
+          name: 'test beggining and tower 2',
+          schedule: { name: 'Salida', datetime: 1648827095557 }
+        },
+        {
+          name: 'test beggining and tower 2',
+          schedule: { name: 'Torre sur', datetime: 1648827095557 }
+        },
+        {
+          name: 'test beggining',
+          schedule: { name: 'Salida', datetime: 1648827095557 }
+        },
+        {
+          name: 'test cathedral and finish',
+          schedule: { name: 'Catedral', datetime: 1648827095557 }
+        },
+        {
+          name: 'test cathedral and finish',
+          schedule: { name: 'Encierro', datetime: 1648827095557 }
+        }
+      ])
+    })
+  })
 });
--- src/app/now/now.page.ts
+++ src/app/now/now.page.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { DBService } from '../db.service';
+import { fakeDB } from '../fake/db';
+import { BrotherHood, DB, Places } from '../model';
+
+type BrotherHoodNow = {
+  name: string
+  schedule: {
+    name: string
+    datetime: number
+  }
+}
 
 @Component({
   selector: 'app-now',
@@ -8,12 +17,44 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./now.page.scss'],
 })
 export class NowPage implements OnInit {
+  brotherhoodsNow: BrotherHoodNow[] = []
+
   constructor(
-    private activatedRoute: ActivatedRoute,
-    private httpClient: HttpClient
+    private dbService: DBService
   ) { }
 
   ngOnInit() {
+    this.dbService.getDB().subscribe(dataBase => {
+      // const brotherhoods = this.concatBrotherhoods(dataBase)
+      // this.getBrotherhoodsNow(brotherhoods, Date.now())
+      const brotherhoods = this.concatBrotherhoods(fakeDB)
+      this.getBrotherhoodsNow(brotherhoods, 1648827095557 - 1000 * 60 * 3)
+    })
+  }
 
+  private concatBrotherhoods(dataBase: DB) {
+    let brotherhoods = []
+    for (let day in dataBase) {
+      brotherhoods = brotherhoods.concat(dataBase[day].brotherhoods)
+    }
+    return brotherhoods
+  }
+
+  private getBrotherhoodsNow(brotherhoods: BrotherHood[], fromDate: number) {
+    const delay = 1000 * 60 * 5 // milliseconds * seconds * minutes
+    brotherhoods.forEach(brotherhood => {
+      for (let datetimeName in brotherhood.datetimes) {
+        const datetime = brotherhood.datetimes[datetimeName]
+        if ((fromDate >= datetime - delay) && (fromDate <= datetime + delay)) {
+          this.brotherhoodsNow.push({
+            name: brotherhood.name,
+            schedule: {
+              name: Places["" + datetimeName],
+              datetime
+            }
+          })
+        }
+      }
+    })
   }
 }
