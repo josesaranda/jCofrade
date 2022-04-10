4d2b2a2f7f4e2fd3d25f8eeae9d73961814d61c8
josesaranda <josesaranda@gmail.com>
2022-04-10 16:09:20 +0200

    Fix import

--- src/app/db.service.ts
+++ src/app/db.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from '../environments/environment';
 import { fakeDB } from './fake/db';
 import { DB } from './model';
 
