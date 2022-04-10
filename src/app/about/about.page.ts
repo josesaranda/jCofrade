323a447f890045db8f026469ae0fd4321535fa19
josesaranda <josesaranda@gmail.com>
2022-04-10 16:10:25 +0200

    Removed unused code

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,19 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.page.html',
   styleUrls: ['./about.page.scss'],
 })
-export class AboutPage implements OnInit {
-  constructor(
-    private activatedRoute: ActivatedRoute,
-    private httpClient: HttpClient
-  ) { }
-
-  ngOnInit() {
-
-  }
-}
+export class AboutPage { }
